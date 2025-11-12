667891d6571dbac91b991a81124ffc07157e04d5
Carmen-SA24 <csalavi871@g.educaand.es>
2025-11-12 12:00:13 +0100

    Editado star.sh

--- devops/Docker/Caronte/dockerfiles/base/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -2,50 +2,13 @@
 # carga las varialbes de entorno pasadas desde el docker-compose.yml
 set -e
 
-check_usuario(){
-  if greo -q "${USUARIO}" /etc/passwd 
-   then
-       echo "${USUARIO} se encuentra en el sistema" >> /root/logs/informe.log
-       return 1
-    else 
-      echo "${USUARIO} no existe en el sistema" >> /root/logs/informe.log
-      return 0
-    fi
-}
-
-check_home(){
-  if [ ! -d /home/${USUARIO} ]
-   then
-       echo "/home/${USUARIO} no existe" >> /root/logs/informe.log
-       return 0 # true
-   else 
-      echo "/home/${USUARIO} existe" >> /root/logs/informe.log
-      return 1 # falso
-  fi
-}
-
-newUser(){
-      check_usuario
-   # 'cat etc/passwd | grep "${USUARIO}"'
-  if [ $? -eq 0 ]
-  then
-     check_home
-     if [ $? -eq 0 ]
-     then
-          useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}   
-          echo "${USUARIO}:${PASSWORD}" | chpasswd
-          echo "Bienvenida ${USUARIO} a tu empresa ..." > /home/${USUARIO}/bienvenida.txt
-          echo "Usuario ${USUARIO} creado correctamente" >> /root/logs/informe.log
-      else
-          echo "Usuario ${USUARIO} no reado, existe home" >> /root/logs/informe.log
-  fi
-  else
-      echo "Usuario ${USUARIO} no creado, existe en passwd" >> /root/logs/informe.log 
-}
+# source /root/admin/base/usuarios/mainUsuarios.sh
 
 main(){
+  # gestion usuario --> getUser.sh
+  # gestion logs --> logrotate.sh
     touch /root/logs/informe.log
-    newUser
+   # newUser
     # encargada de dejar este contenedor vivo en background 
     tail -f /dev/null
 }
