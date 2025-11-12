d3ebadf6c980c84228a975edee42161f1a70b353
Carmen-SA24 <csalavi871@g.educaand.es>
2025-11-12 10:40:01 +0100

    Mensaje descriptivo del cambio

--- devops/Docker/Caronte/dockerfiles/base/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -12,13 +12,35 @@ check_usuario(){
       return 0
     fi
 }
-newUser(){
-  check_usuario
-  if [ $? -eq 0 ]; then
-  useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}   
-  echo "${USUARIO}:${PASSWORD}" | chpasswd
-  echo "Bienvenida ${USUARIO} a tu empresa ..." > /home/${USUARIO}/bienvenida.txt
+
+check_home(){
+  if [ ! -d /home/${USUARIO} ]; then
+   then
+       echo "/home/${USUARIO} no existe" >> /root/logs/informe.log
+       return 0 # true
+   else 
+      echo "/home/${USUARIO} existe" >> /root/logs/informe.log
+      return 1 # falso
   fi
+}
+
+newUser(){
+      check_usuario
+   # 'cat etc/passwd | grep "${USUARIO}"'
+  if [ $? -eq 0 ]
+  then
+     check_home
+     if [ $? -eq 0 ]
+     then
+          useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}   
+          echo "${USUARIO}:${PASSWORD}" | chpasswd
+          echo "Bienvenida ${USUARIO} a tu empresa ..." > /home/${USUARIO}/bienvenida.txt
+          echo "Usuario ${USUARIO} creado correctamente" >> /root/logs/informe.log
+      else
+          echo "Usuario ${USUARIO} no reado, existe home" >> /root/logs/informe.log
+  fi
+  else
+      echo "Usuario ${USUARIO} no creado, existe en passwd" >> /root/logs/informe.log 
 }
 
 main(){
