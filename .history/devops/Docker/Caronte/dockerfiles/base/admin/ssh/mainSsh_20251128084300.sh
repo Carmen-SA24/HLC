8425c2e2db957069a05ace83599b02119c670098
Carmen-SA24 <csalavi871@g.educaand.es>
2025-11-28 08:43:15 +0100

    a

---
+++ .history/devops/Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh_20251128084300.sh
@@ -0,0 +1,10 @@
+make_ssh() {
+    sed -i 's/Port.* /Port '$PORT_SSH'/' /etc/ssh/sshd_config
+    sed -i 's/PermitRootLogin.* /PermitRootLogin no/' /etc/ssh/sshd_config
+
+    service ssh restart
+
+    mkdir -p /home/${USUARIO}/.ssh
+
+    cat /root/admin/base/id_ed25519.pub >> /home/${USUARIO}/.ssh/authorized_keys
+}
