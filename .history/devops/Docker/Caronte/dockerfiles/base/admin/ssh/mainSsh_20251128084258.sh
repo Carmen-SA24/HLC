a00fb24c6ff73fc830ac4703c9fa7ddde30386de
Carmen-SA24 <csalavi871@g.educaand.es>
2025-11-28 08:43:06 +0100

    A

--- .history/devops/Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh_20251128084258.sh
+++ .history/devops/Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh_20251128084258.sh
@@ -1,5 +1,5 @@
 make_ssh() {
-    sed -i 's/Port.* /Port '$PORT_SSH'/' /etc/ssh/sshd_config
+    sed -i 's/Port.* /Port '$PORT_SSH'&/' /etc/ssh/sshd_config
     sed -i 's/PermitRootLogin.* /PermitRootLogin no/' /etc/ssh/sshd_config
 
     service ssh restart
