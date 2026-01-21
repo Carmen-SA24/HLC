dfffd27888ec0b56e590887e5d5293e0dae84ea8
Carmen-SA24 <csalavi871@g.educaand.es>
2026-01-21 16:45:28 +0100

    Upload limpio final v2

--- devops/Docker/Caronte/proyectos/minextjs/deploy/start-web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Iniciar Nginx
-service nginx start
-
-# Iniciar script base de seguridad
-exec /root/admin/base/start.sh
---
+++ devops/Docker/Caronte/proyectos/pcarmen/src/.next/dev/static/chunks/proyectos_nextjs_pages_index_2da965e7._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/8f60e_next_dist_compiled_2fc078b9._.js",
+  "static/chunks/8f60e_next_dist_shared_lib_7b4c0884._.js",
+  "static/chunks/8f60e_next_dist_client_ff53a194._.js",
+  "static/chunks/8f60e_next_dist_50f39502._.js",
+  "static/chunks/8f60e_next_head_10e75145.js",
+  "static/chunks/8f60e_react-dom_59436080._.js",
+  "static/chunks/8f60e_28591607._.js",
+  "static/chunks/[root-of-the-server]__3ef2ec00._.js"
+],
+    source: "entry"
+});
