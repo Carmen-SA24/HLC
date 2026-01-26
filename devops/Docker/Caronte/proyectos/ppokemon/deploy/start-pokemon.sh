b16a746c561ffe25cac4481c6f4bbe8435ee535d
Carmen-SA24 <csalavi871@g.educaand.es>
2026-01-26 19:33:16 +0100

    fix: cambiar ppokemon de Nginx a Node.js para soportar SSR

--- devops/Docker/Caronte/proyectos/ppokemon/deploy/start-pokemon.sh
+++ devops/Docker/Caronte/proyectos/ppokemon/deploy/start-pokemon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Iniciar Nginx
-service nginx start
+# Iniciar Next.js en background
+cd /app && npm start &
 
 # Iniciar script base de seguridad
 exec /root/admin/base/start.sh
+git add -A
